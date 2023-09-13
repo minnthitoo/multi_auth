f4f619bff7533006fd35bfa3df4c7e1ed0c8e0ae
minnthitoo <mto.forlearning@gmail.com>
2023-09-13 09:36:28 +0630

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,27 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // add user
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+        \App\Models\Role::factory()->create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        // add role
+        \App\Models\Role::factory()->create([
+            'name' => 'Admin',
+            'type' => 'admin'
+        ]);
+        \App\Models\Role::factory()->create([
+            'name' => 'User',
+            'type' => 'user'
+        ]);
     }
 }
