eeae2f58a169bd4c83cb36ca8b7e76db689b19be
minnthitoo <mto.forlearning@gmail.com>
2023-09-13 09:31:49 +0630

    edit user and create relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,6 +27,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -58,4 +59,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    // role
+    public function role(){
+        return $this->hasOne(Role::class, 'id', 'role_id'); // id is role id , role_id is user role id
+    }
 }
